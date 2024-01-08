9d4e38fce36b2f7030127ffb218ffdc34266d76b
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2024-01-07 22:16:36 -0700

    added file-loader, image-webpack-loader updated webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -38,8 +38,39 @@ module.exports = {
         type: "asset/resource",
       },
       {
-        test: /\.(png|svg|bmp|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "./assets",
+              name: "[name].[ext]",
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
       },
     ],
   },
